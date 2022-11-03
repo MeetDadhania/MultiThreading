ce71931dcf3f7be7666ebe1b259a3478fa347786
MeetDadhania <meet.dadhania@oneadvanced.com>
2022-11-03 17:58:20 +0530

    Add project files.

---
+++ MultiThreading/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //calling method in single threading diaplay method wait to execute and show method wait display method to execute
+            //ThreadClass.Print();
+            //ThreadClass.Display();
+            //ThreadClass.Show();
+
+            Console.WriteLine("------------------------Main Thread Start----------------------------");
+
+            //Creating Thread for all the methods in ThreadClass 
+            Thread printThread = new Thread(ThreadClass.Print);
+            Thread displayThread = new Thread(ThreadClass.Display);
+            Thread showThread = new Thread(ThreadClass.Show);
+
+            //displayThread don't wait for printThread to execute it start its execution both run simultaneously 
+            printThread.Start();
+
+            Thread.Sleep(1000);
+
+            //suspend thread temporarily
+            printThread.Suspend();
+
+            //sleep for 5 sec
+            Thread.Sleep(5000);
+
+            //resume printThread again
+            printThread.Resume();
+
+            //Using Join on Thread means pausing the calling thread untill the execution of thread is finished on which join is called
+            printThread.Join();
+
+            //wait untill printThread finished its execution
+            displayThread.Start();
+
+            Thread.Sleep(1000);
+
+            //suspend thread temporarily
+            displayThread.Suspend();
+
+            //resume printThread again
+            displayThread.Resume();
+
+            showThread.Start();
+
+
+
+        }
+    }
+}
---
+++ MultiThreading/ThreadClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    //Creating ThreadClass 
+    internal class ThreadClass
+    {
+        //Create static string array
+        static string[] cars = { "Honda", "Ford", "Toyota", "Nissan", "Chevrolet", "BMW", "Kia" };
+        //Create static char array
+        static char[] codes = { 'F', 'R', 'W','Y','M','J','A'};
+
+        //static Print method
+        public static void Print()
+        {
+
+            for(int i = 0; i < 10; i++)
+            {
+                //Print Numbers
+                Console.WriteLine("Print Method is printing : " + i);
+                //add delay of 2 sec when i = 2
+                if(i == 2)
+                {
+                    Thread.Sleep(2000);
+                }
+            }
+        }
+
+        public static void Display()
+        {
+            //print all value in cars array
+            foreach(string car in cars)
+            {
+                //when car = kia add delay of 3 sec
+                if (car == "Kia")
+                {
+                    Thread.Sleep(3000);
+                }
+                Console.WriteLine("Display method is displaying : " + car);
+            }
+        }
+
+        public static void Show()
+        {
+            //print all value of in codes array
+            foreach (char code in codes)
+            {
+                Console.WriteLine("Show method is shows : " + code);
+            }
+        }
+
+    }
+}
